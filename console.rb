85b40753bf43c55f90d8c9f96c9e3921e56cb2cc
Danny Welsh <dannywelsh400@hotmail.com>
2019-04-03 17:41:21 +0100

    creates Album class

--- console.rb
+++ console.rb
@@ -1,17 +1,26 @@
 require('pry-byebug')
 require_relative('models/artist.rb')
+require_relative('models/album.rb')
 
-artist1 = Artist.new({
-  'name' => 'Eminem'
-  })
+artist1 = Artist.new({'name' => 'Eminem'})
 
 artist1.save()
 
-artist2 = Artist.new({
-  'name' => 'AC/DC'
-  })
+artist2 = Artist.new({'name' => 'AC/DC'})
 
 artist2.save()
+
+album1 = Album.new({
+  'title' => 'The Marshall Mathers LP',
+  'genre' => 'Hip-Hop',
+  'artist_id' => "#{artist1.id}"
+  })
+
+album2 = Album.new({
+  'title' => 'Revival',
+  'genre' => 'Hip-Hop',
+  'artist_id' => "#{artist1.id}"
+  })
 
 # artist1.delete()
 # artist1.name = 'John'
